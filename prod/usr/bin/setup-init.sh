d28c0c5e85129e274401bd39f13476147b5a0982
vnmedeiros <vnicius.nm.ba@gmail.com>
2019-07-02 16:00:54 -0300

    change script to install WP core

--- prod/usr/bin/setup-init.sh
+++ prod/usr/bin/setup-init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 wp_path='/var/www/html'
 plugin_wp_path="${wp_path}/wp-content/plugins"
 COMMAND='wp --allow-root --path=/var/www/html'
@@ -15,15 +18,21 @@ if $(${COMMAND} core is-installed); then
   ${COMMAND} option set permalink_structure "/%year%/%monthnum%/%day%/%postname%/"
   ${COMMAND} rewrite flush
 else
-  echo "WP Core is not prepared, creating cache to tainacan plugin!"
-  if [ ! -d "${plugin_wp_path}/tainacan" ]; then
-    url_tainacan='https://downloads.wordpress.org/plugin/tainacan.zip'
-    wget --directory-prefix=${plugin_wp_path} ${url_tainacan}
-    unzip -q ${plugin_wp_path}/tainacan.zip -d ${plugin_wp_path}
-    rm ${plugin_wp_path}/tainacan.zip
-  else
-    echo -e "The tainacan plugin folder has exist!"
-  fi
+  echo "WP Core is not prepared, install WP core!"
+  echo "what the ${bold}URL address${normal} of the new site?"
+  read var_site_url
+  echo "what the ${bold}title${normal} of the new site?"
+  read var_site_title
+  echo "what the ${bold}admin user${normal} of the new site?"
+  read var_site_admin_user
+  echo "what the admin user${bold}e-mail${normal}?"
+  read var_site_admin_email
+  ${COMMAND} core install --url=$var_site_url --title=$var_site_title --admin_user=$var_site_admin_user --admin_email=$var_site_admin_email
+
+  install-tainacan-plugin.sh
+  install-tainacan-theme.sh
+  ${COMMAND} option set permalink_structure "/%year%/%monthnum%/%day%/%postname%/"
+  ${COMMAND} rewrite flush
 fi
 
 if [ "$(id -u)" = '0' ]; then
