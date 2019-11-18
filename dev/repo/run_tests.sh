39cc4ebcfb83af7de7e3e5af96f812f08a704212
Vinícius Nunes <vnicius.nm.ba@gmail.com>
2019-11-18 11:46:42 -0300

    update WP version on tests

--- dev/repo/run_tests.sh
+++ dev/repo/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /repo/tainacan/
-./tests/bin/install-wp-tests.sh $WORDPRESS_DB_TEST $WORDPRESS_DB_USER $WORDPRESS_DB_PASSWORD $WORDPRESS_PATH_TEST $WORDPRESS_DB_HOST 4.9.8
+./tests/bin/install-wp-tests.sh $WORDPRESS_DB_TEST $WORDPRESS_DB_USER $WORDPRESS_DB_PASSWORD $WORDPRESS_PATH_TEST $WORDPRESS_DB_HOST latest
 phpunit 
