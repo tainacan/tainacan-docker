2dc042fb5048333da8e5c7c6c0d1e5334c148ed8
vnmedeiros <vnicius.nm.ba@gmail.com>
2020-02-20 11:32:11 -0300

    remove folder wp-content

--- dev/wp-content/themes/tainacan-interface/tainacan/archive-items.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-<?php get_template_part( 'tainacan/header-collection' ); ?>
-
-	<?php tainacan_the_faceted_search(); ?>
-
-<?php get_footer();
--- dev/wp-content/themes/twentynineteen/tainacan/archive-repository.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-
-<!-- Get the banner to display -->
-<?php get_template_part( 'template-parts/bannerheader' ); ?>
-
-	<?php tainacan_the_faceted_search(); ?>
-
-<?php get_footer(); ?>
