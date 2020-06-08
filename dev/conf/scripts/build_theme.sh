b6d4bd6c1baf5b183b206bf408d689acb768ca83
vnmedeiros <vnicius.nm.ba@gmail.com>
2020-06-08 18:28:30 -0300

    update dev env

---
+++ dev/conf/scripts/build_theme.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /src/tainacan-theme
+./build.sh
--- dev/repo/build_theme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /repo/tainacan-theme
-cp /repo/confs_theme/build-config.cfg /repo/tainacan-theme/
-./build.sh
-chown -R www-data:www-data /var/www/html/wp-content/themes/
