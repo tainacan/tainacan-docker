84d4432ce4408f072c0bdb9e6163066eba85d662
mateuswetah <mateus.m.luna@gmail.com>
2024-09-23 16:24:06 -0300

    Updates settings to better fit later docker version and also updates PHP to 8.3.

--- dev.sh
+++ dev.sh
@@ -2,17 +2,17 @@
 
 function fn_stop {
     echo "[STOP TAINACAN]"
-    sudo docker-compose -f docker-compose.yml down
-    sudo docker-compose -f docker-compose.dev.yml down
-    sudo docker-compose -f docker-compose.nginx.yml down
-    sudo docker-compose -f docker-compose.dev.elastic.yml down
+    docker compose -f docker-compose.yml down
+    docker compose -f docker-compose.dev.yml down
+    docker compose -f docker-compose.nginx.yml down
+    docker compose -f docker-compose.dev.elastic.yml down
 }
 
 function fn_start {
     echo "[START TAINACAN]"
-    sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
-    sudo docker exec -it tainacan_build sh -c "/src_base/scripts/build_plugin.sh --build"
-    sudo docker exec -it tainacan_build sh -c "/src_base/scripts/build_theme.sh"
+    docker compose -f docker-compose.yml -f docker-compose.dev.yml up
+    docker exec -it tainacan_build sh -c "/src_base/scripts/build_plugin.sh --build"
+    docker exec -it tainacan_build sh -c "/src_base/scripts/build_theme.sh"
 }
 
 for i in "$@"
@@ -20,35 +20,35 @@ do
     case $i in
         --build-image)
             echo "[BUILD IMAGE]"
-            sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml build
+            docker compose -f docker-compose.yml -f docker-compose.dev.yml build
             exit
         ;;
         --build-image-nginx)
             echo "[BUILD IMAGE] WITH NGINX"
-            sudo docker-compose -f docker-compose.nginx.yml -f docker-compose.dev.yml docker-compose -f build
+            docker compose -f docker-compose.nginx.yml -f docker-compose.dev.yml docker compose -f build
             exit
         ;;
         --build-image-elastic)
             echo "[BUILD IMAGE WITH ELASTICSEARCH]"
-            sudo docker-compose -f docker-compose.dev.elastic.yml -f docker-compose.dev.yml -build
+            docker compose -f docker-compose.dev.elastic.yml -f docker-compose.dev.yml -build
             exit
         ;;
         --build)
             echo "[BUILD TAINACAN]"
-            sudo docker exec -it tainacan_build sh -c "/src_base/scripts/build_plugin.sh --build"
-            sudo docker exec -it tainacan_build sh -c "/src_base/scripts/build_theme.sh"
+            docker exec -it tainacan_build sh -c "/src_base/scripts/build_plugin.sh --build"
+            docker exec -it tainacan_build sh -c "/src_base/scripts/build_theme.sh"
             exit
         ;;
         --build-prod)
             echo "[BUILD TAINACAN]"
-            sudo docker exec -it tainacan_build sh -c "/src_base/scripts/build_plugin.sh --build-prod"
-            sudo docker exec -it tainacan_build sh -c "/src_base/scripts/build_theme.sh"
+            docker exec -it tainacan_build sh -c "/src_base/scripts/build_plugin.sh --build-prod"
+            docker exec -it tainacan_build sh -c "/src_base/scripts/build_theme.sh"
             exit
         ;;
         --watch-build)
             echo "[BUILD WATCH TAINACAN]"
-            sudo docker exec -it tainacan_build sh -c "/src_base/scripts/build_theme.sh"
-            sudo docker exec -it tainacan_build sh -c "/src_base/scripts/build_plugin.sh --watch-build"
+            docker exec -it tainacan_build sh -c "/src_base/scripts/build_theme.sh"
+            docker exec -it tainacan_build sh -c "/src_base/scripts/build_plugin.sh --watch-build"
             exit
         ;;
         --stop)
@@ -61,33 +61,33 @@ do
         ;;
         --start-nginx)
             echo "[START TAINACAN WUTH NGINX]"
-            sudo docker-compose -f docker-compose.nginx.yml -f docker-compose.dev.yml up
+            docker compose -f docker-compose.nginx.yml -f docker-compose.dev.yml up
             exit
         ;;
         --start-elastic)
             echo "[START TAINACAN WITH ELASTICSEARCH]"
-            sudo sysctl -w vm.max_map_count=262144
-            sudo docker-compose -f docker-compose.dev.elastic.yml -f docker-compose.dev.yml up
+            sysctl -w vm.max_map_count=262144
+            docker compose -f docker-compose.dev.elastic.yml -f docker-compose.dev.yml up
             exit
         ;;
         --run-tests)
             echo "[EXECUTANDO TESTES--PHPUnit]"
-            sudo docker exec -it tainacan_build sh -c "/src_base/scripts/run_tests.sh"
+            docker exec -it tainacan_build sh -c "/src_base/scripts/run_tests.sh"
             exit
         ;;
         --bash)
             echo "[INTO A CONTAINER /bin/bash]"
-            sudo docker exec -it tainacan_build /bin/bash
+            docker exec -it tainacan_build /bin/bash
             exit
         ;;
         --bash-mysql)
             echo "[INTO A CONTAINER OF MYSQL /bin/bash]"
-            sudo docker exec -it tainacan_db /bin/bash            
+            docker exec -it tainacan_db /bin/bash            
             exit
         ;;
         --error-logs)
             echo "[ERRORS LOG tainacan-dev]"
-            sudo docker logs -f tainacan_fpm_apache > /dev/null
+            docker logs -f tainacan_fpm_apache > /dev/null
             exit
         ;;
         --help)
