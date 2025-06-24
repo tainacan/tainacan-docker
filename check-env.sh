2e9a6dfe8d11ff7ec46f0e38e10162966ba0fbae
Henrique Torres <henriquetlandin@gmail.com>
2025-06-24 11:38:45 -0300

    feat: adicionando verificação se o ambiente está pronto ou não para rodar
    Co-authored-by: Pedro Cabeceira <cabeceira2003@gmail.com>

--- check-env.sh
+++ check-env.sh
@@ -15,10 +15,14 @@ log_success() { echo -e "\t  ${GREEN}${BOLD}[OK]${NC} $1"; }
 log_warn()    { echo -e "\t  ${YELLOW}${BOLD}[WARN]${NC} $1"; }
 log_error()   { echo -e "  ${RED}${BOLD}[ERROR]${NC} $1"; }
 
+# Variável para verificar se o ambiente está pronto
+is_error=0
+
 # Função que verifica se o script está sendo executado como root
 check_root() {
     if [ "$EUID" -ne 0 ]; then
-        log_error "Este script precisa ser executado como superusuário (root). Use: sudo $0"
+        log_error "Este script precisa ser executado como superusuário (root)."
+        log_error "Para rodar o script, use: sudo $0"
         exit 1  
     fi
 }
@@ -28,6 +32,7 @@ check_docker() {
     log_info "Verificando se o Docker está instalado..."
     if ! command -v docker > /dev/null 2>&1; then
         log_error "Docker não está instalado. Por favor, instale o Docker antes de continuar."
+        is_error=1
         exit 1
     fi
     log_success "Docker está instalado."
@@ -35,6 +40,7 @@ check_docker() {
     log_info "Verificando se o Docker está em execução..."
     if ! docker info > /dev/null 2>&1; then
         log_error "Docker está instalado, mas não está em execução. Inicie o serviço Docker e tente novamente."
+        is_error=1
         exit 1
     fi
     log_success "Docker está em execução. \n"
@@ -52,14 +58,28 @@ check_ports() {
     for port in 80 3306; do
         if lsof -i :$port | grep LISTEN > /dev/null; then
             log_warn "Porta $port está em uso! (${port_desc[$port]})"
+            is_error=1
         else
             log_success "Porta $port está livre. (${port_desc[$port]})"
         fi
     done
-    log_info "As portas acima precisam estar livres para o ambiente Docker funcionar corretamente."
+}
+
+check_env() {
+    echo
+
+    if [ "$is_error" -gt 0 ]; then
+        log_error "Ambiente não está pronto para rodar!"
+        log_error "Verifique as mensagens de warn acima e corrija os problemas."
+        exit 1
+    else
+        log_success "Ambiente preparado para rodar!"
+        exit 0
+    fi
 }
 
 # Execução das verificações
 check_root
 check_docker
 check_ports
+check_env
